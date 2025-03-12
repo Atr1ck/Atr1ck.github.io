ea6f8c8410636d9991522f29fff9e688ced3f9b0
Atr1ck <3126670240@qq.com>
2025-03-12 16:36:41 +0800

    Updates

--- assets/Test-CCScDitQ.js
+++
@@ -1,3 +0,0 @@
-import{j as t}from"./index-CEXQt1Eq.js";function s(){return t.jsxs("div",{className:"flex items-center justify-center h-full w-full gap-x-3",children:[t.jsx("div",{className:"loading loading-spinner"}),t.jsx("div",{className:`text-6xl font-bold text-white relative 
-                transition-transform hover:translate-y-[-3px]
-                [text-shadow:_4px_4px_0_#000000]`,children:"Loading..."})]})}export{s as default};
